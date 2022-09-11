c48a1d746c3ace77d495b2817018c0e4613a058f
Judith Peter <judith.lourde@gmail.com>
2022-09-11 14:45:35 +0200

    Add employee data

--- src/app/components/employees-list/employees-list.component.ts
+++ src/app/components/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesListComponent implements OnInit {
 
+  employees: Employee[] = [
+    {
+      id: 'CWO9ZHvBTtaB0rfhwhCJnO',
+      name: 'Romin Hanks',
+      email: 'romin.k.irani@gmail.com',
+      phone: 4081234567,
+      salary: 60000,
+      department: 'Human Resource'
+    },
+    {
+      id: 'EJPB8vuKsUQzYmuM7GVGgT',
+      name: 'Neil Irani',
+      email: 'neilrirani@gmail.com',
+      phone: 4081111111,
+      salary: 80000,
+      department: 'Information Technology'
+    },
+    {
+      id: 'kwp71lZJIGHA037EmRwV6e',
+      name: 'Tom Doe',
+      email: 'tomhanks@gmail.com',
+      phone: 4082222222,
+      salary: 40000,
+      department: 'Accounts'
+    },
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    
+
   }
 
 }
